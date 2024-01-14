eef2d58d4884f79c8c6db0cdd98d9bb94706029d
Monalysa <monalysaklauck@gmail.com>
2024-01-13 23:19:05 -0300

    add base backend

---
+++ EasyOpenBank/src/main/java/com/monalysa/EasyOpenBank/EasyOpenBankApplication.java
@@ -0,0 +1,13 @@
+package com.monalysa.EasyOpenBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyOpenBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyOpenBankApplication.class, args);
+	}
+
+}
---
+++ EasyOpenBank/src/test/java/com/monalysa/EasyOpenBank/EasyOpenBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monalysa.EasyOpenBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyOpenBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
